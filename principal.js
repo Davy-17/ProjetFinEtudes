063fc03115ba253b4743ea334a80e5b042c8aa3f
Davy-17 <63070545+Davy-17@users.noreply.github.com>
2023-03-01 19:28:42 -0500

    Ajout d'un slider et de commentaires

--- principal.js
+++ principal.js
@@ -33,3 +33,85 @@ if(window.scrollY > 0){
 
 }
 );
+/*
+
+var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 2000;	// Time Between Switch
+	 
+// Image List
+images[0] = "images/ae.png";
+images[1] = "images/Ai.png";
+images[2] = "images/c4d.png";
+images[3] = "images/realflow.png";
+
+// Change Image
+function changeImg(){
+	document.slide.src = images[i];
+
+	// Check If Index Is Under Max
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  i++; 
+	} else { 
+		// Reset Back To O
+		i = 0;
+	}
+
+	// Run function every x seconds
+	setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
+*/
+
+/*slider*/
+var slides=document.querySelector('.slider-items').children;
+var slideDroite=document.querySelector(".slideDroite");
+var slideGauche=document.querySelector(".slideGauche");
+var totalSlides=slides.length;
+var index=0;
+
+slideDroite.onclick=function () {
+	next("droite");
+}
+slideGauche.onclick=function () {
+	next("gauche");
+}
+
+function next(direction){
+/* quand tu cliques à droite l'index augmente et est remis à zéro pour avoir la première slide */
+  if(direction=="droite"){
+
+	/*Index augmente de 1*/
+	 index++;
+
+		/*L'index se remet à zéro une fois qu'on DÉPASSE la dernière slide (dernier index), on revient à la première slide */
+	  if(index==totalSlides){
+	   index=0;
+	  }
+  } 
+
+/*Si cliques à gauche */
+  else{
+	
+	/*et que la slide est la première, on revient à la dernière slide */
+		  if(index==0){
+		   index=totalSlides-1;
+		  }
+		  /* l'index dimnue de 1 */
+		  else{
+		   index--;
+		  }
+   }
+/*boucle des slides */
+ for(i=0; i<slides.length; i++){
+		 slides[i].classList.remove("active");
+ }
+ slides[index].classList.add("active");     
+
+ console.log(index);
+}
+
+
